1c88de43d95a8040818a5367ed590c1edbb07bb7
Rafiqul Islam <rafiqr169q@gmail.com>
2023-06-23 01:10:01 +0200

    Import articles while system boots

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -17,6 +17,7 @@ php artisan cache:clear
 php artisan config:clear
 php artisan route:clear
 php artisan migrate --seed
+php artisan import:articles --init
 
 php artisan serve --port=$PORT --host=0.0.0.0 --env=.env
 
